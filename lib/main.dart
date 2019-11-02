e5417fe3a38df718533f3cca59c459281268d1cf
Omer <umarg569@gmail.com>
2019-11-02 04:24:26 -0700

    registration_screen_complete

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -42,129 +43,145 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: SafeArea(
-          child: Form(
-            key: registrationform,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,color: Colors.black),
-                  decoration: InputDecoration(hintText: "First Name"),
-                  onChanged: (value){
-                    valueFirstname = value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length < 3){
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(17.0),
+              child: Form(
+                key: registrationform,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,color: Colors.black),
+                      decoration: InputDecoration(hintText: "First Name"),
+                      onChanged: (value){
+                        valueFirstname = value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length < 3){
 
-                      return "Enter a valid name";
-                    }
+                          return "Enter a valid name";
+                        }
 
-                  },
+                      },
 
+                    ),
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,color: Colors.black),
+                      decoration: InputDecoration(hintText: "Last Name"),
+                      onChanged: (value){
+
+                        valueLastName = value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length<3){
+                          return "Enter a valid last name";
+                        }
+
+                      },
+
+
+                    ),
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
+                      decoration: InputDecoration(hintText: "Email"),
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value){
+                        valueEmial= value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length < 6){
+                          return "Enter a valid last name";
+                        }
+
+                      },
+
+
+                    ),
+                    TextFormField(
+                      style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
+                      decoration: InputDecoration(hintText: "Password"),
+                      onChanged: (value){
+
+                        valuePassword= value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length<6){
+
+                          return "Enter a valid password";
+                        }
+
+                      },
+
+                    ),
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
+                      decoration: InputDecoration(hintText: "Confirm Password"),
+                      onChanged: (value){
+                        valueConfirmPassword = value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length<6){
+
+                          return "Enter a valid Password";
+                        }
+                        if(valuePassword != value){
+                          return "password not match";
+                        }
+
+                      },
+                    ),
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black
+                      ),
+                      decoration: InputDecoration(hintText: "Phone no"),
+                      keyboardType: TextInputType.phone,
+                      onChanged: (value){
+
+                        valuePhoneno = value;
+                      },
+                      validator: (value){
+                        if(value == null || value.length != 11){
+
+                          return "Enter a valid phone";
+                        }
+
+                      },
+                    ),
+
+                    SizedBox(
+                      height: 30,
+                    ),
+
+                    RaisedButton(
+                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 35),
+                      child: Text("submit"),
+                      textColor: Colors.black87,
+                      onPressed: (){
+                        if(registrationform.currentState.validate()){
+
+                          print("Button, click");
+
+                        }
+                      },
+                    ),
+                    SizedBox(height: 5,),
+                    RaisedButton(
+                      child: Text("Already have account? Login to continoue"),
+                      onPressed: (){
+                        print("Login button pressed");
+                      },
+
+                    )
+                  ],
                 ),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,color: Colors.black),
-                  decoration: InputDecoration(hintText: "Last Name"),
-                  onChanged: (value){
-
-                    valueLastName = value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length<3){
-                      return "Enter a valid last name";
-                    }
-
-                  },
-
-
-                ),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
-                  decoration: InputDecoration(hintText: "Email"),
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value){
-                    valueEmial= value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length < 6){
-                      return "Enter a valid last name";
-                    }
-
-                  },
-
-
-                ),
-                TextFormField(
-                  style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
-                  decoration: InputDecoration(hintText: "Password"),
-                  onChanged: (value){
-
-                    valuePassword= value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length<6){
-
-                      return "Enter a valid password";
-                    }
-
-                  },
-
-                ),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black),
-                  decoration: InputDecoration(hintText: "Confirm Password"),
-                  onChanged: (value){
-                    valueConfirmPassword = value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length<6){
-
-                      return "Enter a valid Password";
-                    }
-                    if(valuePassword != value){
-                      return "password not match";
-                    }
-
-                  },
-                ),
-                TextFormField(
-                  style: TextStyle(
-                      fontSize: 22,fontWeight: FontWeight.bold,color: Colors.black
-                  ),
-                  decoration: InputDecoration(hintText: "Phone no"),
-                  keyboardType: TextInputType.phone,
-                  onChanged: (value){
-
-                    valuePhoneno = value;
-                  },
-                  validator: (value){
-                    if(value == null || value.length != 11){
-
-                      return "Enter a valid phone";
-                    }
-
-                  },
-                ),
-
-
-                RaisedButton(
-                  child: Text("submit"),
-                  color: Colors.blueAccent,
-                  textColor: Colors.black87,
-                  onPressed: (){
-                    if(registrationform.currentState.validate()){
-
-                      print("Button, click");
-
-                    }
-                  },
-                )
-              ],
+              ),
             ),
           ),
         )
